0fb88a74e4d6da787eaa0299a88e7bbca6ab0898
pabloing93 <pabloing1993@gmail.com>
2023-10-10 09:44:42 -0300

    Creating project

---
+++ functions.py
@@ -0,0 +1,5 @@
+def example(number):
+  return number * 2
+
+def tellmeyourname(name):
+  return f'hi {name}!'
---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib as ml
+from functions import *
+
+print('Hello Data Science!')
+print(example(5)) #defined in functions.py
+print(tellmeyourname('Pablo'))
